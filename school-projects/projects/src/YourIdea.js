970e50174d3b6622e948214f050fa6a95a6aae84
سلطان <gbj6420@gmail.com>
2024-05-04 21:34:58 +0300

    all1

--- school-projects/projects/src/YourIdea.js
+++ school-projects/projects/src/YourIdea.js
@@ -3,14 +3,12 @@ export default function YourIdea(){
         <main className="YourIdea">
             <img src={require("./assets/img1.png")} alt="img"></img>
             <form>
-                <label htmlFor="name">ادخل اسمك الرباعي</label>
-                <input type="text" placeholder="الاسم الرباعي" id="name"></input>
 
                 <label htmlFor="title">عنوان الفكرة</label>
                 <input type="text" placeholder="الفكرة" id="title"></input>
 
                 <label htmlFor="text">نص الفكرة</label>
-                <textarea type="text" placeholder="" id="text"></textarea>
+                <textarea type="text" placeholder="نص الفكرة" id="text"></textarea>
 
                 <label htmlFor="imgs">ارفاق صور</label>
                 <input type="file" name="" value=""  id='imgs'></input>
